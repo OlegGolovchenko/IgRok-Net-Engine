fbb8a702515f71907fd132e2e75dc4d1b16c05c4
Oleg Golovchenko <IgRoK_be@hotmail.com>
2022-08-18 12:59:33 +0200

    adeng more string validation rules

---
+++ IGNEngine/IGNEngine/ValidationRules/StringRules/StringRegexRule.cs
@@ -0,0 +1,41 @@
+﻿//#############################################################################
+//  Igrok-Net Engine
+//  Copyright(C) 2022  Oleg Golovchenko
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <https://www.gnu.org/licenses/>
+//#############################################################################
+
+using System.Text.RegularExpressions;
+
+namespace IGNEngine.ValidationRules.StringRules
+{
+    public class StringRegexRule : ValidationRule
+    {
+        private string regex;
+
+        public StringRegexRule(string regex)
+        {
+            this.regex = regex;
+        }
+
+        public override bool IsValid(object data)
+        {
+            if (data is string)
+            {
+                return Regex.IsMatch((string)data, regex);
+            }
+            return false;
+        }
+    }
+}
--- IGNEngine/IGNEngine/ValidationRules/ValidationRuleChain.cs
+++ IGNEngine/IGNEngine/ValidationRules/ValidationRuleChain.cs
@@ -16,6 +16,7 @@
 //  along with this program.  If not, see <https://www.gnu.org/licenses/>
 //#############################################################################
 
+using IGNEngine.ValidationRules.StringRules;
 using System;
 using System.Collections.Generic;
 
